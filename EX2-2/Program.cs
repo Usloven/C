51cc186d6bc0f392a7c3d4a1d95e563d5dc1852a
usloven <usloven@gmail.com>
2022-11-22 21:43:28 +0300

    зададние 2-2

---
+++ EX2-2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string A = Console.ReadLine();
+char[] arr; 
+arr = A.ToCharArray();
+if (A.Length>=3){
+   Console.WriteLine(arr[2]);
+    }
+else{Console.WriteLine("третьей цифры нет");}
