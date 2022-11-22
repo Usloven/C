c4e17ba7cfbd3de741009c8234b0766aa3da49a0
usloven <usloven@gmail.com>
2022-11-22 18:12:42 +0300

    зададние 2

---
+++ EX3/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int C = Convert.ToInt32(Console.ReadLine());
+int[] Num = {A,B,C};
+
+int max = Num[0];
+for (int i =0 ;i < Num.Length-1;i++)
+{
+    
+    if (max<Num[i+1])
+        {
+            max =  Num[i+1];
+        }
+    
+}
+Console.WriteLine(max);
