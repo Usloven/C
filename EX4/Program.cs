964ab1a700adf20104ee40fd470a3e2aabadee92
usloven <usloven@gmail.com>
2022-11-22 18:29:54 +0300

    зададние 3

---
+++ EX4/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int A = Convert.ToInt32(Console.ReadLine());
+string yes = "да";
+string no = "нет";
+if (A%2>0){
+    Console.WriteLine(no);
+    }
+else {Console.WriteLine(yes);}
