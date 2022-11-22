03ee43bb770745c252b122429f01162ce912d9fe
usloven <usloven@gmail.com>
2022-11-22 22:24:21 +0300

    зададние 2-3

---
+++ EX2-3/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Day = Convert.ToInt32(Console.ReadLine());
+while (Day>7){Console.WriteLine("введите цифру от 1 до 7");
+Day = Convert.ToInt32(Console.ReadLine());
+}
+if (Day<6){
+    Console.WriteLine("нет");
+}
+else {Console.WriteLine("да");}
