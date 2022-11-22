1d8159eafeb14609b535424ec234f57ff78b0cb3
usloven <usloven@gmail.com>
2022-11-22 16:08:15 +0300

    первое задание из дз

---
+++ EX2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (A>=B)  
+{ max = A;}
+else
+{
+    max = B;
+}
+Console.WriteLine(max);
