78fd4b9e35dbbd7fc97af7036205c2b487eae04c
usloven <usloven@gmail.com>
2022-11-23 21:11:18 +0300

    зададние 3-1

---
+++ EX3-1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more informations
+string Palindrom (string Num1)
+{
+     string Answer = string.Empty;
+for (int i=0;i<Num1.Length;i++)
+{
+   
+    if (Num1[i]==Num1[Num1.Length-1-i])
+    {;
+    Answer = "да";
+    }
+    else {
+        Answer = "нет";
+        break;
+    }
+    
+}
+return Answer;
+}
+
+Console.WriteLine(Palindrom(Console.ReadLine()));
