2ff8d3f2b3f855229d9434e5ef755e0d1f83bfd1
usloven <usloven@gmail.com>
2022-11-25 20:03:27 +0300

    зададние 3-3 copy

---
+++ EX3-3 copy/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more informations
+string Cube(string Num) 
+{
+    string Result = "";
+    int Amount = Convert.ToInt32(Num);
+    for (int i =0; i<Amount; i++)
+    {
+        Result = Result+" "+Convert.ToString(Math.Pow((i+1),3));
+    }
+    
+    
+    return Result;
+}
+
+Console.WriteLine(Cube(Console.ReadLine()));
