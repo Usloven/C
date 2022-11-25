699599799abc2409b558e8f3384865ddd98ae2d9
usloven <usloven@gmail.com>
2022-11-25 19:09:35 +0300

    зададние 3-3укerror

---
+++ EX3-3/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more informations
+int[] Cube(int Num) 
+{
+    int [] Result = new int[Num+1];
+    for (int i =0; i<Num; i++)
+    {
+        Result[i] = Result[i+1]*Result[i+1]*Result[i+1];
+    }
+    
+    
+    return Result;
+}
+
+Console.WriteLine(Cube(2));
+// Console.WriteLine(Cube(Convert.ToInt32(Console.ReadLine())));
