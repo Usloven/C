9f921a0f03145e839a88c1632c1e3698719908e9
usloven <usloven@gmail.com>
2022-11-29 13:09:30 +0300

    заа4-2

---
+++ EX4-2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+// int NumberSum(string Num) 
+// {
+//     int Result = 0;
+//     for (int i =0; i<Num.Length; i++)
+//     {
+       
+//         Result = Result + (Num[i]);
+        
+//         }
+    
+
+//     return Result;
+// }
+
+//  Console.Write(NumberSum(Console.ReadLine()));
+
+// почему то такой вариант не работет. Пример 234 число выдает  153. Ошибка вроде в 8 строке (на этом моменте что-то идет неправильно, но не понял что)
+
+
+int NumberSum(int Num) 
+{
+    int Result = 0;
+
+    while (Num>0)
+    {
+        Result = Result + Num%10;
+        Num = Num/10;
+    }
+    
+
+    return Result;
+}
+
+ Console.Write(NumberSum(122));
+
+
