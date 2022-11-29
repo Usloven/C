4c9bd728416c273d07f4be927371884c6ee16f74
usloven <usloven@gmail.com>
2022-11-29 15:16:30 +0300

    4-1

---
+++ EX4-1 copy/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Pow (int A, int B)
+{
+    int result = 1;
+    for (int i=0; i<B; i++)
+    {
+        result = result* A;
+    }
+    return result;
+}
+
+Console.WriteLine(Pow(2,10));
