ff8cbf81196eedb633f2524d4d94886201cfc245
usloven <usloven@gmail.com>
2022-11-29 15:09:06 +0300

    4-3

---
+++ EX4-3/Program.cs
@@ -0,0 +1,13 @@
+﻿void PrintArr (int[] Array)
+{
+      for (int i=0;i<Array.Length;i++)
+      {
+        Array[i] = new Random().Next(1,10);
+            
+      }
+      Console.WriteLine(String.Join(",",Array));
+} 
+
+int[] nums = new int[8];
+PrintArr(nums);
+
