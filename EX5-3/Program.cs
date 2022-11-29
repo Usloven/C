a1bc2979386760f777654428fe56f69b02417840
usloven <usloven@gmail.com>
2022-11-29 16:09:45 +0300

    5-3

---
+++ EX5-3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+
+void FillArr (int[] Array)
+{
+      for (int i=0;i<Array.Length;i++)
+      {
+        Array[i] = new Random().Next(1,10);
+            
+      }
+      Console.WriteLine(String.Join(",",Array));
+} 
+
+void ArrdifMaxMin (int[] Array)
+{
+    int result =0;
+    int max = 0;
+    int min = Array[0];
+    for (int i=0;i<Array.Length;i++)
+      {
+        if(Array[i]>max)
+        {
+          max=Array[i];
+        }
+      else if (Array[i]<min)
+        {
+          min = Array[i];
+        }
+      }
+      result = max-min;
+      Console.WriteLine(result);
+
+}
+int[] nums = new int[5];
+FillArr(nums);
+ArrdifMaxMin(nums);
