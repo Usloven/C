7218bc6351fc1f35d2a87135d67c561200f1875f
usloven <usloven@gmail.com>
2022-11-29 15:55:59 +0300

    5-2

---
+++ EX5-2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+void FillArr (int[] Array)
+{
+      for (int i=0;i<Array.Length;i++)
+      {
+        Array[i] = new Random().Next(1,10);
+            
+      }
+      Console.WriteLine(String.Join(",",Array));
+} 
+
+void ArrSumNotDivPosision (int[] Array)
+{
+    int result =0;
+    for (int i=0;i<Array.Length;i=i+2)
+      {
+        result = result + Array[i];
+      }
+      Console.WriteLine(result);
+
+}
+int[] nums = new int[5];
+FillArr(nums);
+ArrSumNotDivPosision(nums);
